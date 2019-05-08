d647fcf289ef0954bb1c04f3e2b7bd923206fb9a
= <=>
2019-05-08 21:40:47 +0100

    added comment header

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,5 +1,14 @@
 package server;
 
+/*
+This is an app that allows you to create a mock server for API calls
+Simply place the mapping ie the request URL and which JSON file it should bring back
+Once the server is started you can use the cucumber framework that calling the urls defined here
+and you will be returned the response mapped
+
+ */
+
+
 import server.MockServer;
 
 import java.io.IOException;
